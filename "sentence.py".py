e14a31b544322c159c4d5dc419132f34a514b63b
laurakim2020 <31958630+laurakim2020@users.noreply.github.com>
2017-09-18 21:01:32 -0400

    Add files via upload

---
+++ "sentence.py".py
@@ -0,0 +1,33 @@
+word1 = "All"
+word2 = "work"
+word3 = "and"
+word4 = "no"
+word5 = "play"
+word6 = "makes"
+word7 = "Jack"
+word8 = "a"
+word9 = "dull"
+word10 = "boy"
+print(word1 + word2 + word3 + word4 + word5 + word6 + word7 + word8 + word9 + word10)
+
+print(6*(1-2))
+
+#dogsarecute
+
+bruce=6
+print(bruce+4)
+
+answer=input("Years?")
+P = 10000 
+r = 8/100 
+n = 12 
+t = float(answer) 
+Amn = P*(1+r/n)**(n*t) 
+print("Your final amount is:", Amn)
+
+#7 cannot divide by 0
+
+
+
+
+
