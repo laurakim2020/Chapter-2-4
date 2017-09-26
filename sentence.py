fd8c457caf1aa57e3257fbfe0eb4e1c12b7d00ea
laurakim2020 <31958630+laurakim2020@users.noreply.github.com>
2017-09-26 08:06:51 -0400

    Update sentence.py

--- sentence.py
+++ sentence.py
@@ -10,22 +10,7 @@ word9 = "dull"
 word10 = "boy"
 print(word1 + word2 + word3 + word4 + word5 + word6 + word7 + word8 + word9 + word10)
 
-print(6*(1-2))
 
-#dogsarecute
-
-bruce=6
-print(bruce+4)
-
-answer=input("Years?")
-P = 10000 
-r = 8/100 
-n = 12 
-t = float(answer) 
-Amn = P*(1+r/n)**(n*t) 
-print("Your final amount is:", Amn)
-
-#7 cannot divide by 0
 
 
 
