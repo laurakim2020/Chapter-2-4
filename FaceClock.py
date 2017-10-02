8bcae72b059df352a31df817b9e27dd357b5b753
laurakim2020 <31958630+laurakim2020@users.noreply.github.com>
2017-10-02 14:21:12 -0400

    Add files via upload

---
+++ FaceClock.py
@@ -0,0 +1,22 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+tess = turtle.Turtle()
+tess.color("blue")
+tess.pensize(3)
+
+tess.stamp
+tess.shape("turtle")
+for i in range(12):
+    tess.stamp()
+    tess.penup()
+    tess.forward(20)
+    tess.pendown()
+    tess.forward(150)
+    tess.penup()
+    tess.forward(20)
+    tess.setpos(0,0)
+    tess.left(360/12)
+tess.hideturtle()
+    
+   
